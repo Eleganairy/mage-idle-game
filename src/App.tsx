f4193bee93a71ac639aeb9450e04c68e2c7c579f
Eleganairy <61018157+Eleganairy@users.noreply.github.com>
2025-10-14 17:23:43 +0200

    Added basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Grid } from '@mui/material'
+import { PageHeader } from './header'
+import { ActionBar } from './action-bar'
+import { ActiveScreen } from './active-screen'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Grid container sx={{ height: '100vh', width: '100vw' }}>
+      {/* Header */}
+      <Grid size={12} sx={{ height: '10%', backgroundColor: '#1976d2' }}>
+        <PageHeader />
+      </Grid>
+      {/* Sidebar and Main Content */}
+      <Grid container size={12} sx={{ height: 'calc(100vh - 10%)' }}>
+        <Grid size={3} sx={{ height: '100%', backgroundColor: '#43a047' }}>
+          <ActionBar />
+        </Grid>
+        <Grid size={9} sx={{ height: '100%', backgroundColor: '#fbc02d' }}>
+          <ActiveScreen />
+        </Grid>
+      </Grid>
+    </Grid>
   )
 }
 
---
+++ src/active-screen/active-screen.tsx
@@ -0,0 +1,3 @@
+export const ActiveScreen = () => {
+    return <div>Active Screen</div>
+}
